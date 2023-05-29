d92c24084d6af193c1c5188ac047cd00ce17019e
paichiwo <lzerucha@gmail.com>
2023-05-29 11:15:06 +0100

    Update converter.py with ffmpeg functionality.
    ffmpeg.exe chosen as the main handler for the application.
    application will depend on ffmpeg.exe being in the root folder

--- converter.py
+++ converter.py
@@ -1,44 +1,33 @@
 import PySimpleGUI as psg
 import os
-import ffmpeg
-
-import os
-import ffmpeg
+import subprocess
 
 
-def convert_to_wmv(file):
+def convert_to_mp4(file, ffmpeg_path):
     name, ext = os.path.splitext(file)
-    count = 1
-    out_name = name + "_convert_{}{}".format(count, ext)
-
-    while os.path.exists(out_name):
-        count += 1
-        out_name = name + "_convert_{}{}".format(count, ext)
-
-    def progress_callback(progress):
-        print("Converting {}: {:.2f}%".format(file, progress))
-
-    ffmpeg.input(file).output(out_name, vcodec='wmv1', progress=progress_callback).run()
-    print("Finished converting {}".format(file))
+    out_name = name + "_convert.wmv"
+    command = [ffmpeg_path, '-i', file, '-c:v', 'wmv1', out_name]
+    subprocess.call(command)
+    print(f"Finished converting {file}")
 
 
 psg.theme("DarkTeal2")
-layout = [[psg.T("")], [psg.Text("Choose a file: "), psg.Input(), psg.FileBrowse(key="-IN-")], [psg.Button("Submit")]]
+layout = [
+    [psg.T("")],
+    [psg.Text("Choose a file: "), psg.Input(), psg.FileBrowse(key="-IN-")],
+    [psg.Button("Submit")]
+]
 
 # Building Window
-window = psg.Window('My File Browser', layout, size=(1000, 150))
+window = psg.Window('My File Browser', layout, size=(600, 150))
 
+ffmpeg_path = 'executables/ffmpeg'  # ffmpeg path
 
 while True:
     event, values = window.read()
     if event == psg.WIN_CLOSED or event == "Exit":
         break
     elif event == "Submit":
-        try:
-            convert_to_wmv(values["-IN-"])
-            break
-        except ffmpeg._run.Error:
-            print("codec error")
+        convert_to_mp4(values["-IN-"], ffmpeg_path)
 
 window.close()
-
