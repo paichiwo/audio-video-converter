efecb14e65c994f96d8d6427e6be11437e8e33ea
paichiwo <lzerucha@gmail.com>
2023-06-03 18:30:10 +0100

    Change print statements to message box communication. Clean code for readability.

--- converter.py
+++ converter.py
@@ -8,7 +8,7 @@ def calculate_progress(file_path, progress_bar):
     total_size = os.path.getsize(file_path)
     bytes_processed = 0
 
-    with open(file_path, 'rb') as file:
+    with open(file_path, "rb") as file:
         while True:
             chunk = file.read(4096)  # Read a chunk of data from the file
             if not chunk:
@@ -16,7 +16,6 @@ def calculate_progress(file_path, progress_bar):
 
             bytes_processed += len(chunk)
             percentage = (bytes_processed / total_size) * 100
-
             # Update the progress bar value
             progress_bar.update(round(percentage))
 
@@ -29,20 +28,20 @@ def use_ffmpeg(input_file, output_file, video_codec, progress_bar):
         choice = psg.popup_yes_no("Overwrite?")
         if choice != 'Yes':
             # User does not want to overwrite, exit the function
-            print("Conversion canceled.")
+            window["-MESSAGE-"].update("Conversion canceled.")
             return
 
-    ffmpeg_command = ['executables/ffmpeg', '-i', input_file, '-c:v', video_codec, '-y', output_file]
+    ffmpeg_command = ["executables/ffmpeg", "-i", input_file, "-c:v", video_codec, "-y", output_file]
     process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     output, error = process.communicate()
     calculate_progress(output_file, progress_bar)
     if process.returncode == 0:
         # Conversion successful
-        print("Conversion completed successfully!")
+        window["-MESSAGE-"].update("Conversion completed successfully!")
     else:
-        print(output)
-        print(error)
-        print(process.returncode)
+        window["-MESSAGE-"].update(output)
+        window["-MESSAGE-"].update(error)
+        window["-MESSAGE-"].update(process.returncode)
 
 
 def converter(video_codec, file_extension):
@@ -53,7 +52,7 @@ def converter(video_codec, file_extension):
     try:
         use_ffmpeg(input_file, output_file, video_codec, progress)
     except FileNotFoundError:
-        print("No ffmpeg found")
+        window["-MESSAGE-"].update("No ffmpeg found")
 
 
 psg.theme("DarkBlue")
@@ -70,13 +69,13 @@ layout = [
         psg.Push()
     ],
     [psg.Push(), psg.Button("Submit", border_width=0), psg.Push()],
-    [psg.Text("", key="-MESSAGE-")],
+    [psg.Push(), psg.Text("", key="-MESSAGE-"), psg.Push()],
     [psg.VPush()],
-    [psg.ProgressBar(100, orientation='h', expand_x=True, size=(20, 2), key='-PBAR-')]
+    [psg.ProgressBar(100, orientation="h", expand_x=True, size=(20, 2), key="-PROGRESS-BAR-")]
 ]
 
 # Building Window
-window = psg.Window('Video Converter', layout, size=(600, 150))
+window = psg.Window("Video Converter", layout, size=(600, 150))
 
 codec = ""
 extension = ""
@@ -86,30 +85,29 @@ while True:
     if event == psg.WIN_CLOSED or event == "Exit":
         break
 
-    elif event == '-BROWSE-':
-        file = psg.popup_get_file('', no_window=True)
-        window['-IN-'].update(file)
+    elif event == "-BROWSE-":
+        file = psg.popup_get_file("", no_window=True)
+        window["-IN-"].update(file)
 
     elif event == "Submit":
         if values["-IN-"]:
-            progress = window["-PBAR-"]  # Get the progress bar element
+            progress = window["-PROGRESS-BAR-"]  # Get the progress bar element
             if values["-WMV-"]:
-                codec = 'wmv1'
-                extension = '.wmv'
+                codec = "wmv1"
+                extension = ".wmv"
             elif values["-MP4-"]:
-                codec = 'h264'
-                extension = '.mp4'
+                codec = "h264"
+                extension = ".mp4"
             elif values["-AVI-"]:
-                codec = 'libx264'
-                extension = '.avi'
+                codec = "libx264"
+                extension = ".avi"
             elif values["-MP3-"]:
-                codec = 'libmp3lame'
-                extension = '.mp3'
+                codec = "libmp3lame"
+                extension = ".mp3"
 
             converter(codec, extension)
 
         else:
-            print("No file")
-
+            window["-MESSAGE-"].update("No file selected.")
 
 window.close()
