0321aadac294f577e83db909bafffc814659b7bc
paichiwo <lzerucha@gmail.com>
2023-06-03 17:46:56 +0100

    Add frame_3 and add exclusion for .ini files

--- drag&drop/main.py
+++ drag&drop/main.py
@@ -19,7 +19,7 @@ def get_all_paths(event):
             folder_path = event.data
             for file in glob.glob(f"{folder_path}/*.*"):
                 attributes = os.stat(file).st_file_attributes
-                if not os.path.basename(file).startswith('.') and attributes != 6:
+                if not os.path.basename(file).startswith('.') and attributes != 6 and not file.endswith(".ini"):
                     files.append(file)
         else:  # Handle a single or multiple file drop
             for file in event.data.split():
@@ -36,6 +36,7 @@ def update_listbox(event):
     if len(file_list) > 0:
         frame_1.pack_forget()
         frame_2.pack()
+        frame_3.pack()
 
 
 def clear_listbox():
@@ -58,10 +59,12 @@ image = tk.PhotoImage(file='drop.png')  # Drag & Drop image
 image_label = Label(frame_1, image=image, padx=5, pady=5)
 
 frame_2 = tk.Frame()
-
-ClearButton = Button(frame_2, text="Clear", command=clear_listbox)
-FolderButton = Button(frame_2, text="Destination", command=folder_selection)
 ListboxWidget = Listbox(frame_2, width=100)
+
+frame_3 = tk.Frame()
+ClearButton = Button(frame_3, text="Clear", command=clear_listbox)
+FolderButton = Button(frame_3, text="Destination", command=folder_selection)
+
 
 # Place widgets
 image_label.grid(row=0, column=0, columnspan=3, sticky="news", padx=5, pady=5)
