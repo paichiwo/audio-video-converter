a4a5f067b6ce114f40d4c106ac3f9f5dbc3ffe7e
paichiwo <lzerucha@gmail.com>
2023-07-09 13:45:28 +0100

    Add data to data.py
    info_header
    info_text
    github_link

--- data.py
+++ data.py
@@ -3,3 +3,30 @@
 version = "1.0"
 font = "Calibri"
 colors = ['#087e8b', '#ff5a5f', '#3c3c3c', '#f5f5f5']
+
+github_link = "https://github.com/paichiwo/"
+
+
+info_header = f"""
+AUDIO VIDEO CONVERTER v{version}
+by Paichiwo
+2023
+"""
+
+info_text = """
+Thank you for your interest in my Audio/Video Converter application. 
+Application is coded in Python and it's utilizing 'FFmpeg' capabilities.
+
+What is FFmpeg?
+FFmpeg is a powerful open-source software library and command-line tool 
+used for handling multimedia data. It can decode, encode, transcode, mux, 
+demux, stream, and filter various audio and video formats. 
+http://ffmpeg.org/
+
+Features:
+Convert audio files such as MP3, WAV, AAC, FLAC, etc. between audio formats.
+Convert video files such as MP4, AVI, MKV, MOV, etc. to other video or audio.
+
+Future plans:
+Process multiple audio/video files simultaneously, saving time and effort.
+"""
