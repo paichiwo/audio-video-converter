a3e88b059f9ddf7e280d219217cc6e4f2fc87bfa
paichiwo <lzerucha@gmail.com>
2023-07-25 21:45:16 +0100

    Create info_window.py and settings_window.py with UI elements for each window

---
+++ src/settings_window.py
@@ -0,0 +1,116 @@
+import json
+import src.config as data
+from tkinter import Tk, filedialog, Label, PhotoImage, Button
+from src.helpers import get_downloads_folder_path
+
+
+def showsettings():
+    """Create a new tkinter window with settings for the application"""
+
+    def get_output_path():
+        """Get a file path for the chosen output folder"""
+        folder_selected = filedialog.askdirectory()
+        output_path_label.configure(text=folder_selected)
+        return
+
+    def save_settings():
+        output_folder = output_path_label.cget('text')
+        with open('../data/settings.json', 'w') as file:
+            json.dump({'output_folder': output_folder}, file)
+        settings_info_label.configure(text="Settings saved")
+
+    def load_settings():
+        try:
+            with open('../data/settings.json', 'r') as file:
+                settings = json.load(file)
+                return settings['output_folder']
+        except json.decoder.JSONDecodeError:
+            output_folder = get_downloads_folder_path()
+            return output_folder
+
+    sett = Tk()
+    sett.geometry("480x250")
+    sett.title("Settings")
+    sett.iconbitmap('./images/audio-video_converter_icon_512x512.ico')
+    sett.configure(bg=data.colors[2])
+    sett.resizable(False, False)
+
+    settings_header = Label(
+        sett,
+        text="SETTINGS\n\n",
+        font=(data.font, 13, 'bold'),
+        fg=data.colors[3],
+        bg=data.colors[2],
+        justify='center')
+    settings_header.pack()
+
+    output_path_info = Label(
+        sett,
+        text="Choose output folder:",
+        font=(data.font, 11),
+        fg=data.colors[3],
+        bg=data.colors[2],
+        justify='center')
+    output_path_info.pack()
+
+    output_path_image = PhotoImage(
+        master=sett,
+        file='../images/filepath.png')
+    output_path_image_label = Label(
+        sett,
+        image=output_path_image,
+        bg=data.colors[2],
+        borderwidth=0)
+    output_path_image_label.pack()
+
+    folder_icon_image = PhotoImage(
+        master=sett,
+        file='../images/folder_icon.png')
+    folder_icon_label = Label(
+        sett,
+        image=folder_icon_image,
+        bg=data.colors[0])
+    folder_icon_label.place(x=44, y=123)
+
+    output_path_label = Label(
+        sett,
+        text=load_settings(),
+        font=(data.font, 10),
+        fg=data.colors[3],
+        bg=data.colors[0],
+        justify='left')
+    output_path_label.place(x=75, y=123)
+
+    output_path_button_image = PhotoImage(
+        master=sett,
+        file='../images/plus_16x16.png')
+    output_path_button = Button(
+        sett,
+        image=output_path_button_image,
+        bg=data.colors[0],
+        activebackground=data.colors[0],
+        borderwidth=0,
+        command=get_output_path)
+    output_path_button.place(x=410, y=126)
+
+    save_button_image = PhotoImage(
+        master=sett,
+        file="../images/save_button.png")
+    save_button = Button(
+        sett,
+        image=save_button_image,
+        bg=data.colors[2],
+        activebackground=data.colors[2],
+        borderwidth=0,
+        command=save_settings)
+    save_button.pack()
+
+    settings_info_label = Label(
+        sett,
+        text="",
+        font=(data.font, 10),
+        fg=data.colors[3],
+        bg=data.colors[2])
+    settings_info_label.place(x=10, y=278)
+
+    sett.mainloop()
